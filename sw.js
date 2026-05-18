c6b9f725f4f5192bcff84c3c67e3ca994b9168d0
Revu7 <revustore@gmail.com>
2026-05-19 00:37:28 +0200

    sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,9 @@
-// Service worker desactivado
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', () => self.clients.claim());
+self.addEventListener('fetch', event => {
+  event.respondWith(fetch(event.request));
+});
+self.addEventListener('notificationclick', event => {
+  event.notification.close();
+  event.waitUntil(clients.openWindow('/'));
+});
